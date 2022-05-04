134984a63ba9809eb81f401a9082d96679048c1b
Sávio Da Costa Santos <saviocosta.flam20@gmail.com>
2022-05-03 23:17:59 -0300

    Modificação para classes abstratas

---
+++ src/main/kotlin/one/digitalinovation/diobankkotlinpoo/Funcionario.kt
@@ -0,0 +1,12 @@
+package one.digitalinovation.diobankkotlinpoo
+
+import java.math.BigDecimal
+
+abstract class Funcionario(
+    nome: String,
+    cpf: String,
+    val salario: BigDecimal
+) : Pessoa(nome, cpf) {
+
+    abstract fun calculoAuxilio()
+}
---
+++ src/main/kotlin/one/digitalinovation/diobankkotlinpoo/teste/TestePessoa.kt
@@ -0,0 +1,18 @@
+package one.digitalinovation.diobankkotlinpoo.teste
+
+import one.digitalinovation.diobankkotlinpoo.Funcionario
+import one.digitalinovation.diobankkotlinpoo.Pessoa
+import java.math.BigDecimal
+
+fun main() {
+
+//    val savio = Pessoa(nome = "Sávio", cpf = "021-587-693-06")
+//    println("Nome: ${savio.nome}\nCPF: ${savio.cpf}")
+
+    println()
+
+//    val marilia = Funcionario(nome = "Marília", cpf = " 021-523-458-08", salario = BigDecimal.valueOf(2000.0))
+//    println("Nome: ${marilia.nome}\nCPF: ${marilia.cpf}\nSalario: ${marilia.salario}")
+
+
+}
